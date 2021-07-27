7dc9dffb454564b86d993e1f13610ced9e07ac80
Rafael Pereira de Melo <63520209+rafaelpdemelo@users.noreply.github.com>
2021-07-27 20:09:46 -0300

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -1,11 +1,20 @@
-let listaOriginal = document.querySelector('ul.mt-4')
-var novaLista = document.createElement('li')
-var novoA = document.createElement('a')
-var novoSpan = document.createElement('span')
-novoSpan.textContent = 'Nosso Site'
-listaOriginal.appendChild(novaLista)
-novaLista.appendChild(novoA)
-novoA.appendChild(novoSpan)
-novoA.classList.add('nav-link')
-novoA.href = 'https://matematicanoplural.com.br'
-novaLista.classList.add('mt-4', 'nav-item-site')
+let listaOriginal = document.querySelector('ul.mt-4');
+var novaLista = document.createElement('li');
+var novoA = document.createElement("a");
+var novaDiv = document.createElement("div");
+var novoSpan = document.createElement("div");
+var novoI = document.createElement("i");
+
+
+listaOriginal.appendChild(novaLista);
+novaLista.classList.add("mt-4", "nav-item-simulados");
+novaLista.appendChild(novoA);
+novoA.classList.add("nav-link");
+novoA.href = "https://estuda.com"
+novoSpan.textContent = 'Simulados'
+novoA.appendChild(novaDiv);
+novoA.appendChild(novoSpan);
+novoSpan.classList.add("ml-2.5");
+novaDiv.classList.add("flex-none", "w-5", "h-5");
+novaDiv.appendChild(novoI);
+novoI.setAttribute('data-feather', 'check-square');
